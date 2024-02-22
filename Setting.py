8ef5f955cb5f2e8e78af98851f14dd4a739919ae
Mengjie <43809054+mengjiejia@users.noreply.github.com>
2024-02-22 15:17:52 -0500

    Add files via upload

---
+++ Setting.py
@@ -0,0 +1,16 @@
+import os
+'''Things that we can change'''
+###################################################
+siam_model = 'siamapn'
+###################################################
+# dataset_name = 'V4RFlight112'
+dataset_name = 'UAV123'
+##################################################
+video_name = ''
+#########################################################################################
+project_path = '/home/mengjie/PycharmProjects/AttackSiamTracker-main'
+dataset_root = '/home/mengjie/PycharmProjects/AttackSiamTracker-main/pysot/test_dataset'
+train_set_path = '/home/louis/Ad^2Attack/GOT10K_reproduce'
+
+# general model (siam) path
+# /home/mengjie/PycharmProjects/Ad2Attack/snapshot/general_model.pth
